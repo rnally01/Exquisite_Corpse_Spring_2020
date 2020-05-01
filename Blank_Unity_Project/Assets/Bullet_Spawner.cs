3431217d19267d0d88513ed6cec1ff0620770f13
dreino10 <dreino10@gmail.com>
2020-05-01 15:28:55 -0400

    Bullet Fires
    added the ability to shoot a bullet

---
+++ Blank_Unity_Project/Assets/Bullet_Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Spawner : MonoBehaviour
+{
+    public GameObject projectile;
+    private GameObject newProjectile;
+    public Transform firePosition;
+    public float bulletForce = 500F;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool shoot = Input.GetButtonDown("Fire1");
+
+        if (shoot)
+        {
+            Debug.Log("Firing left mouse click");
+
+            newProjectile = Instantiate(projectile, firePosition.transform.position, firePosition.transform.rotation) as GameObject;
+            newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
+        }
+    }
+}
