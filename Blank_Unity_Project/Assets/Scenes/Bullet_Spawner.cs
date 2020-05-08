b0bb8609d212add930befb58d814982e2ef63d3f
RoomyMango <65043379+RoomyMango@users.noreply.github.com>
2020-05-08 19:57:45 -0400

    Add files via upload
    Heavy edits to the scene (walls, lights, a stack of boxes with rigid bodies)
    right-click spawns box with rigid body

---
+++ Blank_Unity_Project/Assets/Scenes/Bullet_Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Spawner : MonoBehaviour
+{
+    public GameObject projectile;
+    private GameObject newProjectile;
+    public GameObject box;
+    private GameObject newbox;
+    public Transform firePosition;
+    public float bulletForce = 500F;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool shoot = Input.GetButtonDown("Fire1");
+        bool deploy = Input.GetButtonDown("Fire2");
+
+        if (shoot)
+        {
+            Debug.Log("Firing left mouse click");
+
+            newProjectile = Instantiate(projectile, firePosition.transform.position, firePosition.transform.rotation) as GameObject;
+            newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
+        }
+
+        if (deploy)
+        {
+            Debug.Log("Firing right mouse click");
+
+            newbox = Instantiate(box, firePosition.transform.position, firePosition.transform.rotation) as GameObject;
+        }
+    }
+}
