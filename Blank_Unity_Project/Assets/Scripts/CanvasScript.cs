c91d22d94aef9805aab95a2295936842c18427b1
nbiglin <63429451+nbiglin@users.noreply.github.com>
2020-05-08 00:39:24 -0400

    Added Devito Coins
    Added Collectable Coins and a Canvas with basic instructions and coin counter

---
+++ Blank_Unity_Project/Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour
+{
+    public Text coinAmount;
+    private GameObject[] coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coins = GameObject.FindGameObjectsWithTag("Coin");
+
+        if (coins.Length == 5)
+        {
+            coinAmount.text = "x0";
+        }
+        if (coins.Length == 4)
+        {
+            coinAmount.text = "x1";
+        }
+        if (coins.Length == 3)
+        {
+            coinAmount.text = "x2";
+        }
+        if (coins.Length == 2)
+        {
+            coinAmount.text = "x3";
+        }
+        if (coins.Length == 1)
+        {
+            coinAmount.text = "x4";
+        }
+        if (coins.Length == 0)
+        {
+            coinAmount.text = "x5";
+        }
+    }
+}
