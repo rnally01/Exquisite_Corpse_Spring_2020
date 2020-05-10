06297152df8ea9cfd110bd90d788428f4db526b6
JackB28 <61855057+JackB28@users.noreply.github.com>
2020-05-10 19:26:51 -0400

    Add files via upload

--- Blank_Unity_Project/Assets/Scripts/Bullet_Spawner.cs
+++ Blank_Unity_Project/Assets/Scripts/Bullet_Spawner.cs
@@ -8,7 +8,13 @@ public class Bullet_Spawner : MonoBehaviour
     private GameObject newProjectile;
     public Transform firePosition;
     public float bulletForce = 500F;
+    private AudioSource Audio;
+    [SerializeField] private AudioClip MainShoot;
 
+    private void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -18,8 +24,11 @@ public class Bullet_Spawner : MonoBehaviour
         {
             Debug.Log("Firing left mouse click");
 
+            
             newProjectile = Instantiate(projectile, firePosition.transform.position, firePosition.transform.rotation) as GameObject;
             newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
+            Audio.clip = MainShoot;
+            Audio.Play();
         }
     }
 }
--- Blank_Unity_Project/Assets/Scripts/NickSchmitts/BulletDie.cs
+++ Blank_Unity_Project/Assets/Scripts/NickSchmitts/BulletDie.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BulletDie : MonoBehaviour
 {
+    private GameObject TeleportationPoint;
+    private AudioSource Audio;
+    [SerializeField] private AudioClip BoxBullyHit;
 	private void Start()
 	{
-		Destroy(gameObject, 3f);
+		Audio = GetComponent<AudioSource>();
 	}
+
+    private void Awake()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "BullyBox")
+        {
+            Audio.clip = BoxBullyHit;
+            Audio.Play();
+            Destroy(col.gameObject);
+        }
+
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
 }
