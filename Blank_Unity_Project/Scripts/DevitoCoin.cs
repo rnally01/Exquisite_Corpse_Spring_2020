a8d916df044eea2be0d9052c5586bed903cffe61
JackB28 <61855057+JackB28@users.noreply.github.com>
2020-05-10 19:09:27 -0400

    Added Sound Effects and a Game Over screen
    I added sound effects to enemies, and when you shoot a bullet. I also added a game over screen when a player gets shot.

---
+++ Blank_Unity_Project/Scripts/DevitoCoin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevitoCoin : MonoBehaviour
+{
+    public GameObject player;
+    private BoxCollider coin;
+
+    public GameObject pickUpSound;
+    private GameObject newPickupSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coin = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            newPickupSound = Instantiate(pickUpSound, transform.position, transform.rotation) as GameObject;
+            Destroy(gameObject);
+        }
+    }
+}
