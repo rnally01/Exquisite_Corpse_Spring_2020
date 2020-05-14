d2d8dd6c5fb2e09056a1440699e38f49131be41b
rnally01 <robert.nally@gmail.com>
2020-05-14 00:04:49 -0400

    Revert "test merge commit"
    This reverts commit e8e0c8564752da9dce9faf07190b634ea2dc19a8.

---
+++ Blank_Unity_Project/Assets/Scenes/TriggerSFX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSFX : MonoBehaviour
+{
+	public AudioSource playSound;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		playSound.Play();
+	}
+}
